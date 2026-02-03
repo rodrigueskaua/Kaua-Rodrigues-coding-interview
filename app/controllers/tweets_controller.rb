080bfd61181ec418ce9b0c261d5f63bace19a88f
Kauã Rodrigues <kauarodrigues.fn@gmail.com>
2026-02-03 10:58:56 -0300

    feat: pagination for tweets index endpoint

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,13 @@
 class TweetsController < ApplicationController
   def index
+    page = params[:page]&.to_i || 1
+    per_page = params[:per_page]&.to_i || 5
+    
     tweets = Tweet.by_user(search_params[:user_id])
-  
+                  .offset((page - 1) * per_page)
+                  .limit(per_page)
+                  .order(created_at: :asc)
+
     render json: tweets.all
   end
   
